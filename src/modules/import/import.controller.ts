43c711fdab91b78d464aae8d6d542f123b095e2e
hellomraza <hellomraza@gmail.com>
2026-05-02 15:26:12 +0530

    fix(import): remove type annotation for file parameter in import method

--- src/modules/import/import.controller.ts
+++ src/modules/import/import.controller.ts
@@ -103,7 +103,7 @@ export class ImportController {
         .addMaxSizeValidator({ maxSize: MAX_FILE_SIZE })
         .build({ errorHttpStatusCode: HttpStatus.BAD_REQUEST }),
     )
-    file: Express.Multer.File,
+    file,
     @Req() request: any,
   ) {
     // Validate file exists
